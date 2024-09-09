ccc1d654afab2a5ed94c40822c6cdc1846096515
Alexandre Arruda <alexandre.monteiro.bec@gmail.com>
2024-09-09 10:00:41 -0300

    add parcipant form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native'
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { styles } from './styles'
 
 export function Home() {
@@ -6,6 +6,18 @@ export function Home() {
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
       <Text style={styles.eventDate}>Quinta, 5 de Setembro de 2024.</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
