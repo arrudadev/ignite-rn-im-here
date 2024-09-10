19ba6eb126a55d2e69808202907b51b5f3b57517
Alexandre Arruda <alexandre.monteiro.bec@gmail.com>
2024-09-10 12:38:32 -0300

    setup jest

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/styles.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/react-native/extend-expect'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation)',
+  ],
+}
+
+export default config
